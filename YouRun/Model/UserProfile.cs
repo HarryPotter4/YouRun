f5cdcffde85491e701a0fedf62dc61ab38f5d813
Marius Becherer <marius.becherer@gmx.net>
2016-04-05 15:42:16 +0200

    inital commit

---
+++ YouRun/Model/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouRun.Model
+{
+    class UserProfile
+    {
+        private String aspiredGoal;
+        private DateTime expiredDate;
+        private String aspiredTime;
+    }
+}
