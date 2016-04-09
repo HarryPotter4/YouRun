09ec02784d07a4b545fc92fa3fd527b3463adec5
Marius Becherer <marius.becherer@gmx.net>
2016-04-09 13:55:20 +0200

    Add Bindings to buttons

--- YouRun/ViewModels/MainViewModel.cs
+++ YouRun/ViewModels/MainViewModel.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using System.Diagnostics;
 
 namespace YouRun.ViewModels
 {
     class MainViewModel : Screen
     {
+        public void btn_To_Current_Body_Data()
+        {
+            Debug.WriteLine("Button Next clicked!");
+            
+        }
+
+        public void btn_To_Individual()
+        {
+            Debug.WriteLine("Button Next clicked!");
+        }
+        public void btn_Back_To_Goals()
+        {
+
+        }
+        public void btn_back_To_Body_Data()
+        {
+
+        }
+        public void btn_generate_Workout_Plan()
+        {
+
+        }
 
     }
 }
