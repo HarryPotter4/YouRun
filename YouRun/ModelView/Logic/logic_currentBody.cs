f396b0bb11e6127c61f21aa691bd449621a5fcf5
preissdavid.de@gmail.com <preissdavid.de@gmail.com>
2016-04-12 11:18:19 +0200

    add some functions like schlafempehlung, ...

--- YouRun/ModelView/Logic/logic_currentBody.cs
+++ YouRun/ModelView/Logic/logic_currentBody.cs
@@ -15,10 +15,15 @@ namespace YouRun.ModelView.Logic
         double weight = 0;
         double pace = 0;
         double level = 1;
+        int distance = 0;
+        int currentWeeklyDistance = 0;
         int nOTPW = 1; // numberofTrainingPerWeek
         double powerFactorWoman = 1.5; //Leistungsunterschied Männer/Frauen
-        double weeklsDistance;
+        int leistungsfaktorGlobal = 1;
 
+        int zusätlicherSchlaf=0; // zusätzlich benötigter Schlaf in Minuten
+         
+      
 
         public void setGenderfactor()
         {
@@ -27,6 +32,40 @@ namespace YouRun.ModelView.Logic
                 genderfactor = powerFactorWoman;
             }
         }
+
+        public double getWeeklyDistance(double leistungsfaktor)
+        {
+            return leistungsfaktor * distance;
+        }
+
+        public double schaetzeMarathonZeit(double benötigeZeitfuerLauf, bool Lauf10km) //Hierzu sollte noch die GUI angepasst werden.
+        {
+            if (Lauf10km)
+            {
+                return benötigeZeitfuerLauf * 4.667;
+            }
+            else
+            {
+                return benötigeZeitfuerLauf * 9.798;
+            }
+        }
+
+        public void schlafempfehlung()
+        {
+            if (10 < (getWeeklyDistance(leistungsfaktorGlobal)-currentWeeklyDistance) && getWeeklyDistance(leistungsfaktorGlobal) < 20 )
+            {
+                zusätlicherSchlaf = 15;
+            }
+            else if (getWeeklyDistance(leistungsfaktorGlobal)>=20 && getWeeklyDistance(leistungsfaktorGlobal)<30)
+            {
+                zusätlicherSchlaf = 30;
+            }
+            else if (getWeeklyDistance(leistungsfaktorGlobal)>=30)
+            {
+                zusätlicherSchlaf = 45; 
+            }
+        }
+
 
 
     }
