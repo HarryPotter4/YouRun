058c863a834102b8990d90b869eb07f7143fe95f
preissdavid.de@gmail.com <preissdavid.de@gmail.com>
2016-04-20 12:58:31 +0200

    Test Method added

---
+++ Calculator.Tests/CalculatorOpTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    class CalculatorOpTest
+    {
+        [TestMethod]
+        public void ShouldAddReturnNineWhenPassFiveAndFour()
+        {
+            //Arrange
+            CalulatorOp sut = new CalulatorOp();
+            //Act
+            int result = sut.add(5, 4);
+            //Assert
+            Assert.AreEqual(9, result);
+        }
+
+            [TestMethod]
+        public void ShouldMulReturnTwentyWhenPassFiveAndFour()
+        {
+            //Arrange
+            CalulatorOp sut = new CalulatorOp();
+            //Act
+            int result = sut.mul(5, 4);
+            //Assert
+            Assert.AreEqual(9, result);
+
+        }
+    }
+}
---
+++ YouRun/TDD/CalcOp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouRun.TDD
+{
+    public class CalcOp
+    {
+        public int add(int num1, int num2)
+        {
+            int result= num1 + num2;
+            return result;
+        }
+
+        public int mul(int num1, int num2)
+        {
+            int result = num1 + num2; //wrong
+            return result;
+        }
+
+    }
+}
